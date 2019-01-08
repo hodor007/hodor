952d7199df21503569bfc0a2e991458c099af0a3
zhengpeng <zhengpeng@tuniu.com>
2019-01-08 10:29:10 +0800

    提交项目

---
+++ src/main/java/com/zp/Application.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C) 2006-2019 Tuniu All rights reserved
+ */
+package com.zp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ * Date: 2019-01-08
+ *
+ * @author zhengpeng
+ */
+//@ComponentScan("com.zp")
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/zp/controller/testController.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) 2006-2019 Tuniu All rights reserved
+ */
+package com.zp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description:
+ * Date: 2019-01-08
+ *
+ * @author zhengpeng
+ */
+@RestController
+@RequestMapping("/hello")
+public class testController {
+
+    @RequestMapping(value = "/world",method = RequestMethod.GET)
+    public String testHello() {
+        return "helloWord";
+    }
+
+}
