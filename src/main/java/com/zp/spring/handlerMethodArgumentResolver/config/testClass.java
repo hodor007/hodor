0f24aeb5247b5607b8f1c0af49b3681f3cbfc7e5
zhengpeng <zhengpeng@tuniu.com>
2019-01-18 09:31:36 +0800

    @JsonCompoment

---
+++ src/main/java/com/zp/spring/handlerMethodArgumentResolver/config/testClass.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C) 2006-2019 Tuniu All rights reserved
+ */
+package com.zp.spring.handlerMethodArgumentResolver.config;
+
+/**
+ * Description:
+ * Date: 2019-01-18
+ *
+ * @author zhengpeng
+ */
+public class testClass {
+}
