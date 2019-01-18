43bd3929d41df5723c437e095dca8c458b731cfd
zhengpeng <zhengpeng@tuniu.com>
2019-01-18 10:12:15 +0800

    git revert测试

--- src/main/java/com/zp/spring/handlerMethodArgumentResolver/config/test1Class.java
+++ src/main/java/com/zp/spring/handlerMethodArgumentResolver/config/test1Class.java
@@ -12,6 +12,16 @@ package com.zp.spring.handlerMethodArgumentResolver.config;
 public class test1Class {
     private String name;
 
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
