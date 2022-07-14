f27add65d9ca1828decda2a8d8565612689dc03a
zhengpeng <320123dy>
2022-07-14 16:33:33 +0800

    python test1

--- src/main/java/com/zp/python/__init__.py
+++ src/main/java/com/zp/python/__init__.py
@@ -1,4 +1,5 @@
 import decimal
+import copy
 
 if __name__ == '__main__':
     # 正常的浮点型有精度问题
@@ -50,5 +51,43 @@ if __name__ == '__main__':
     # for each in "FishC":
     #     print(each)
 
-    # for i in range(5,10,2):
+    # for i in range(5,10,2): # 创建整数列表 range(3) = range(0,3) 是0,1,2,3
     #     print(i)
+
+    # 数组
+    # temp = [1,2,3,4,5,"heihei"]
+    # # temp[-1]
+    # # temp[:3]
+    # # temp[3:]
+    # # temp[0:5:2]
+    # temp.append("孤寡")
+    # temp.extend(["a","b","c"])
+    # temp[temp(s):]=[7,8,9]
+    # temp.insert[1,2] # 在1的位置添加2
+    # temp.remove("heihei") #之后删除匹配到的第一个，指定元素不存在会报错
+    # temp.pop(2) #移除位置2的元素
+    # temp.clear() #清除数组
+    # temp * 3 # 重复3次  [[0]] * 3 生成的会指向同一个[0]
+    # 二维数组
+    # matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    # for i in matrix:
+    #     for each in i:
+    #         print(each, end=' ')
+    #     print()
+    # matrix[1][2]
+
+    # x = "peng"
+    # y = "peng"
+    # x = [1,2,3]
+    # y = [1,2,3] 内存地址不同 所以is结果为false  字符数是指向了统一地址块
+    # print(x is y)
+
+    # 浅拷贝
+    # x = [1,2,3]
+    # y = x.copy()  # 等同于 y = x[:]    copy.copy(x)
+    # 深拷贝 子对象也会拷贝
+    # y = copy.deepcopy(x)
+    # oho = [1,2,3,4]
+    # oho = [i * 2 for i in oho] #列表推导式，创建新列表赋给之前   expression for target in iterable
+    # print(oho)
+    s = []
